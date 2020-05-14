834ccfdfb7eb1b4707c0d5af62ebb4eaf8042d73
ayodkay <ayodelekayode51@yahoo.com>
2020-05-14 02:04:03 -0300

    added sleep and infinite loop

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from selenium import webdriver
+import time
 
 
 class FreeWifi:
     def __init__(self):
-        self.browser = webdriver.   Chrome()
+        self.browser = webdriver.Chrome()
 
     def set_up(self):
         self.browser.get(url='http://www.msftconnecttest.com/redirect')
-        self.browser.maximize_window()
 
     def login_user(self):
         self.set_up()
         self.browser.find_element_by_id('formVideo').submit()
+        time.sleep(120)
+        self.browser.close()
 
 
 if __name__ == '__main__':
-    free = FreeWifi()
+    while True:
+        free = FreeWifi()
 
-    free.login_user()
+        free.login_user()
+
+        time.sleep(960)
